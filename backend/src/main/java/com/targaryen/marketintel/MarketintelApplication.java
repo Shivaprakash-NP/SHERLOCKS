29b915b658bd977f39a2ffbf8e0e0e0e49c0f153
Shivaprakash NP <shivaprakashnp1@gmail.com>
2026-03-25 14:50:18 +0530

    Initial hackathon project structure

---
+++ backend/src/main/java/com/targaryen/marketintel/MarketintelApplication.java
@@ -0,0 +1,13 @@
+package com.targaryen.marketintel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketintelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketintelApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/targaryen/marketintel/MarketintelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.targaryen.marketintel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketintelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
